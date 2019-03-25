2f36ac0152a4590945cc3db76b0dd12a46bf83e4
jouny <dz4tune@gmail.com>
2019-03-25 09:36:00 +0300

    made size dynamic

--- main.cpp
+++ main.cpp
@@ -43,10 +43,40 @@ public:
 
 };
 
+template <typename T>
+struct Array3D {
+    T* ptr;
+    size_t X, Y, Z;
 
-template <unsigned int wcount = 80, unsigned int hcount = 60>
-class Game{
+    Array3D(size_t X, size_t Y, size_t Z) : X(X), Y(Y), Z(Z) {
+        ptr = new T[X*Y*Z];
+    }
+
+    ~Array3D(){
+        delete[] ptr;
+    }
+
+    T get(size_t x, size_t y, size_t z) {
+        return ptr[x*X*Z + y*Z + z];
+    }
+
+    void set(size_t x, size_t y, size_t z, T value) {
+        ptr[x*X*Z + y*Z + z] = value;
+    }
+
+    T* data() {
+        return ptr;
+    }
+
+    size_t len() {
+        return X * Y * Z;
+    }
+};
+
+
+class Game {
 public:
+    size_t width, height;
     // order is important !
     gl::Library lib;        // has to be first (calls glwfinit in constructor and glfwterminate in destructor)
     gl::Window window;      // calls glewinit and also I need to be able to call window.use() before constructing vao and program
@@ -54,36 +84,35 @@ public:
     gl::VertexArray vao;    // needs opengl context which window.use() does
     gl::Program program;    // same as vao
 
-    GLfloat vertices[hcount][wcount][3];
+    Array3D<GLfloat> vertices;
 
     unsigned int cell_size = 1;
 
 
-    Game() : Game(10) {}
-
-
-    explicit Game(unsigned int cell_size) :
+    explicit Game(unsigned int width, unsigned int height, unsigned int cell_size) :
+            width(width),
+            height(height),
+            vertices(width, height, 3),
             cell_size(cell_size),
-            window(cell_size * wcount, cell_size * hcount, "Game of life"),
-            game(wcount, (window.use(), hcount)),
+            window(cell_size * width, cell_size * height, "Game of life"),
+            game(width, (window.use(), height)),
             program(gl::Program::fromFiles("../shaders/vertex.glsl", "../shaders/fragment.glsl"))
     {
 
         // cells should be displayed as squares
         // here we calculate height and width in interval -1, 1 in order not to allow overlapping
-        GLfloat relative_x_cell_size = 2.0f/wcount,
-                relative_y_cell_size = 2.0f/hcount;
+        GLfloat relative_x_cell_size = 2.0f/width,
+                relative_y_cell_size = 2.0f/height;
 
         // position of the first cell
         GLfloat x_start = relative_x_cell_size/2 - 1,
                 y_start = relative_y_cell_size/2 - 1;
-
         // initial values of vertices
-        for (unsigned int y = 0; y < hcount; ++y) {
-            for (unsigned int x = 0; x < wcount; ++x) {
-                vertices[y][x][0] = x_start + x * relative_x_cell_size;
-                vertices[y][x][1] = y_start + y * relative_y_cell_size;
-                vertices[y][x][2] = static_cast <float> (game.cell(x, y));
+        for (unsigned int y = 0; y < height; ++y) {
+            for (unsigned int x = 0; x < width; ++x) {
+                vertices.set(y, x, 0, x_start + x * relative_x_cell_size);
+                vertices.set(y, x, 1, y_start + y * relative_y_cell_size);
+                vertices.set(y, x, 2, static_cast <float> (game.cell(x, y)));
             }
         }
         vao.set_layout(GL_ARRAY_BUFFER, {{3, GL_FLOAT, GL_FALSE}});
@@ -93,19 +122,19 @@ public:
 
 
     void copy_cells(){
-        for (unsigned int y = 0; y < hcount; ++y)
-            for (unsigned int x = 0; x < wcount; ++x)
-                vertices[y][x][2] = static_cast <float> (game.cell(x, y));
+        for (unsigned int y = 0; y < height; ++y)
+            for (unsigned int x = 0; x < width; ++x)
+                vertices.set(y, x, 2, static_cast <float> (game.cell(x, y)));
     }
 
 
     void copy_vao(){
-        vao.copy(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+        vao.copy(GL_ARRAY_BUFFER, vertices.len() * sizeof(GLfloat), vertices.data(), GL_STREAM_DRAW);
     }
 
 
     void draw_vao(){
-        vao.draw(GL_POINTS, 0, hcount * wcount);
+        vao.draw(GL_POINTS, 0, height * width);
         window.swap_buffers();
     }
 
@@ -165,13 +194,13 @@ public:
     std::pair<unsigned int, unsigned int> get_cursor_cell_coords(){ // coordinates in game
         auto&& [x, y] = window.get_cursor_pos();
         x /= cell_size;
-        y = hcount - y / cell_size;
+        y = height - y / cell_size;
         return {x, y};
     };
 
 
     void invert_cell(unsigned int x, unsigned int y){
-        vertices[y][x][2] = game.cell(x, y) = not game.cell(x, y);
+        vertices.set(y, x, 2, game.cell(x, y) = not game.cell(x, y));
         copy_vao();
         draw_vao();
     }
@@ -181,7 +210,7 @@ public:
         game.glider(x, y);
         for (unsigned int Y = y; Y <= y+2; ++Y)
             for (unsigned int X = x; X <= x+2; ++X)
-                vertices[Y][X][2] = static_cast <float> (game.cell(X, Y));
+                vertices.set(Y, X, 2, static_cast <float> (game.cell(X, Y)));
         copy_vao();
         draw_vao();
     }
@@ -207,11 +236,12 @@ public:
 
 
 int main(){
-    Game<100, 80> g(10); // too large template parameters will lead to segfault if allocating on stack but not on heap
+    Game g(100, 80, 10);
     g.glider(10, 50);
     g.glider(10, 40);
     g.glider(20, 50);
     g.glider(20, 40);
+    g.randomize();
     g.play();
 
     return 0;
