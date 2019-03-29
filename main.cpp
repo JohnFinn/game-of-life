7471f90e36a6b3f674aa348e09ba3d5f040dc7ee
Jouni Sunnari <Jouny@tuta.io>
2019-03-30 01:26:42 +0300

    output gl version in main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-
 #include "OpenGlGameOfLife/Game.h"
 
 
 int main(){
     Game g(100, 80, 10);
+    std::cout << glGetString(GL_VERSION);
     g.glider(10, 50);
     g.glider(10, 40);
     g.glider(20, 50);
